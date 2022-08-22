ae931e706791c5643ed793c17c6105bdb9127806
Khurram Zafar <khurram_zafar@hotmail.com>
2022-08-22 20:42:14 +0300

    Chapter 2 (variables): Exercise 4

---
+++ 02-variables/ex04-type_and_value.py
@@ -0,0 +1,7 @@
+width = 17
+height = 12.0
+
+print("width//2: Value:",width//2,"| Type:",type(width//2))     # 8 | int
+print("width//2: Value:",width/2.0,"| Type:",type(width/2.0))   # 8.5 | float
+print("width//2: Value:",height/3,")| Type:",type(height/3))    # 4.0 | float
+print("width//2: Value:",1 + 2 * 5,"| Type:",type(1 + 2 * 5))   # 11 | int
