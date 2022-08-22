e5889cfc8374eb12cfa3e269346caf43b59352e7
Khurram Zafar <khurram_zafar@hotmail.com>
2022-08-22 21:15:19 +0300

    Chapter 3 (conditionals): Exercise 2

---
+++ 03-conditionals/ex02-compute_pay_try_except.py
@@ -0,0 +1,17 @@
+hours = input("Enter Hours: ")
+
+try:
+    hours = int(hours)
+    
+    rate = input("Enter Rate: ")
+
+    rate = float(rate)
+
+    if hours > 40:
+        pay = (40 * rate) + ((hours - 40) * (rate * 1.5))
+    else:
+        pay = hours * rate
+
+    print("Pay", pay)
+except:
+    print("Error, please enter numeric input")
