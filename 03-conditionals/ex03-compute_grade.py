c02d4af5e279a618eb73bb97acc638dc0afcfcdd
Khurram Zafar <khurram_zafar@hotmail.com>
2022-08-23 11:12:58 +0300

    Chapter 3 (conditionals): Exercise 3

---
+++ 03-conditionals/ex03-compute_grade.py
@@ -0,0 +1,22 @@
+from distutils.log import error
+
+
+score = input("Enter score: ")
+
+try:
+    score = float(score)
+
+    if score < 0 or score > 1.0:
+        raise error
+    elif score >= 0.9:
+        print("A")
+    elif score >= 0.8:
+        print("B")
+    elif score >= 0.7:
+        print("C")
+    elif score >= 0.6:
+        print("D")
+    else:
+        print("F")
+except:
+    print("Bad score")
